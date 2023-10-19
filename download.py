9fb6c6c6d36ddc275ec3f75f3318ea12aec5fdc9
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2023-10-20 00:05:20 +0330

    Update and rename dawnload.py to download.py

--- download.py
+++ download.py
@@ -1,14 +1,14 @@
 import requests
-class dawnload :
+class download :
     def __init__(self,Link,Format):
        self.Link=Link
        self.Format=Format 
-    def dawnload(self):
+    def download(self):
         data=requests.get(self.Link).content
         Formatfile=f'./data.{self.Format}'
         with open(Formatfile , "wb") as f:
             f.write(data)
 Link=input("please enter your link :")
 Format=input ("please enter your format :")
-dawnload = dawnload(Link,Format)
-dawnload.dawnload()
+download = download(Link,Format)
+download.download()
