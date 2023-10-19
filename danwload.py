eebb5fd28d34c12926ab9e8e28a9ea2a8445f9d7
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2023-10-20 00:27:55 +0400

    Add files via upload

---
+++ danwload.py
@@ -0,0 +1,22 @@
+import requests
+class dawnload :
+    def __init__(self,Link,Format):
+       self.Link=Link
+       self.Format=Format 
+    def dawnload(self):
+        data=requests.get(self.Link).content
+        Formatfile=f'./data.{self.Format}'
+        with open(Formatfile , "wb") as f:
+            f.write(data)
+Link=input("please enter your link :")
+Format=input ("please enter your format :")
+dawnload = dawnload(Link,Format)
+dawnload.dawnload()
+
+
+
+        
+        
+        
+
+
